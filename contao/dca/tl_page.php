0889aaa1dcb3bd7d1073e4ac83534fef2b2325d7
Sebastian Zoglowek <55794780+zoglo@users.noreply.github.com>
2026-04-20 17:23:22 +0200

    Append the ID in the company selection

--- contao/dca/tl_page.php
+++ contao/dca/tl_page.php
@@ -7,7 +7,7 @@ use Contao\CoreBundle\DataContainer\PaletteManipulator;
 $GLOBALS['TL_DCA']['tl_page']['fields']['dd_company'] = [
     'inputType' => 'select',
     'exclude' => true,
-    'foreignKey' => 'tl_company.name',
+    'foreignKey' => "tl_company.CONCAT(name, ' (ID: ', id, ')')",
     'eval' => ['includeBlankOption' => true, 'tl_class' => 'w50 clr'],
     'sql' => ['type' => 'integer', 'unsigned' => true, 'default' => 0],
     'relation' => ['type' => 'hasOne', 'load' => 'lazy'],
