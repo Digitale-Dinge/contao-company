0ea9873199efe0b847b1e0e8d77dd52c5358df64
Sebastian Zoglowek <55794780+zoglo@users.noreply.github.com>
2026-06-25 17:37:34 +0200

    Allow getting a proper map for serialized data

--- src/Twig/Global/CompanyVariable.php
+++ src/Twig/Global/CompanyVariable.php
@@ -63,33 +63,81 @@ readonly class CompanyVariable
         return StringUtil::deserialize($this->company->get($id)?->closing_times, true);
     }
 
+    /** ToDo: Deprecated, to be replaced with the map variant! use getPhone_numbers_map() instead */
     public function getPhone_numbers(int|null $id = null): array
     {
+        trigger_deprecation('digitaledinge/company-bundle', '0.x', 'Using "%s()" will change its return value, use "%s_map()" for now.', __METHOD__, __METHOD__);
+
         return StringUtil::deserialize($this->company->get($id)?->phone_numbers, true);
     }
 
+    public function getPhone_numbers_map(int|null $id = null): array
+    {
+        return array_column(StringUtil::deserialize($this->company->get($id)?->phone_numbers, true), 'phone');
+    }
+
+    /** ToDo: Deprecated, to be replaced with the map variant! use getEmails_map() instead */
     public function getEmails(int|null $id = null): array
     {
+        trigger_deprecation('digitaledinge/company-bundle', '0.x', 'Using "%s()" will change its return value, use "%s_map()" for now.', __METHOD__, __METHOD__);
+
         return StringUtil::deserialize($this->company->get($id)?->emails, true);
     }
 
+    public function getEmails_map(int|null $id = null): array
+    {
+        return array_column(StringUtil::deserialize($this->company->get($id)?->emails, true), 'email');
+    }
+
+    /** ToDo: Deprecated, to be replaced with the map variant! use getWebsites_map() instead */
     public function getWebsites(int|null $id = null): array
     {
+        trigger_deprecation('digitaledinge/company-bundle', '0.x', 'Using "%s()" will change its return value, use "%s_map()" for now.', __METHOD__, __METHOD__);
+
         return StringUtil::deserialize($this->company->get($id)?->websites, true);
     }
 
+    public function getWebsites_map(int|null $id = null): array
+    {
+        return array_column(StringUtil::deserialize($this->company->get($id)?->websites, true), 'website');
+    }
+
+    /** ToDo: Deprecated, to be replaced with the map variant! use getFax_numbers_map() instead */
     public function getFax_numbers(int|null $id = null): array
     {
+        trigger_deprecation('digitaledinge/company-bundle', '0.x', 'Using "%s()" will change its return value, use "%s_map()" for now.', __METHOD__, __METHOD__);
+
         return StringUtil::deserialize($this->company->get($id)?->fax_numbers, true);
     }
 
+    public function getFax_numbers_map(int|null $id = null): array
+    {
+        return array_column(StringUtil::deserialize($this->company->get($id)?->fax_numbers, true), 'fax');
+    }
+
+    /** ToDo: Deprecated, to be replaced with the map variant! use getSocials_map() instead */
     public function getSocials(int|null $id = null): array
     {
+        trigger_deprecation('digitaledinge/company-bundle', '0.x', 'Using "%s()" will change its return value, use "%s_map()" for now.', __METHOD__, __METHOD__);
+
         return StringUtil::deserialize($this->company->get($id)?->socials, true);
     }
 
+    public function getSocials_map(int|null $id = null): array
+    {
+        return array_column(StringUtil::deserialize($this->company->get($id)?->socials, true), 'url', 'social');
+    }
+
+    /** ToDo: Deprecated, to be replaced with the map variant! use getAdditional_map() instead */
     public function getAdditional(int|null $id = null): array
     {
+        trigger_deprecation('digitaledinge/company-bundle', '0.x', 'Using "%s()" will change its return value, use "%s_map()" for now.', __METHOD__, __METHOD__);
+
         return StringUtil::deserialize($this->company->get($id)?->additional, true);
+    }
+
+    public function getAdditional_map(int|null $id = null): array
+    {
+        return array_column(StringUtil::deserialize($this->company->get($id)?->additional, true), 'value', 'key');
     }
 }
